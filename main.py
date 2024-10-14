d9946141329267aadb4d5cb414a9a8031f468521
Christoph <kissmannchristoph@gmail.com>
2024-10-14 17:58:42 +0200

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #https://realpython.com/python-gui-tkinter/
+#https://janelbrandon.medium.com/setting-up-a-python-development-environment-2e18447cbc24
 import tkinter as tk
 window = tk.Tk()
 greeting = tk.Label(text="Hello, Tkinter")
