4cfa6c0332094f3e828f0b74a875ca0b9a690ff5
Christoph <kissmannchristoph@gmail.com>
2024-10-14 23:53:06 +0200

    test other build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 -m nuitka --mingw64 --onefile --assume-yes-for-downloads --output-dir=build --script-name=src/app.py --enable-plugin=tk-inter --windows-disable-console
+python3 -m nuitka --onefile --assume-yes-for-downloads --output-dir=build --script-name=src/app.py --enable-plugin=tk-inter
