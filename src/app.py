722d2872d8787abf67642f73ba579f1fca9052f0
Christoph <kissmannchristoph@gmail.com>
2024-10-16 20:38:30 +0200

    make things bad

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
-# https://realpython.com/python-gui-tkinter/
-# https://janelbrandon.medium.com/setting-up-a-python-development-environment-2e18447cbc24
-# https://www.freecodecamp.org/news/how-to-setup-virtual-environments-in-python/
+#https://realpython.com/python-gui-tkinter/
+#https://janelbrandon.medium.com/setting-up-a-python-development-environment-2e18447cbc24
+#https://www.freecodecamp.org/news/how-to-setup-virtual-environments-in-python/
 import tkinter as tk
-window = tk.Tk()
+window = tk.Tk();
 greeting = tk.Label(text="Hello, Tkinter")
 greeting.pack()
 window.mainloop()
